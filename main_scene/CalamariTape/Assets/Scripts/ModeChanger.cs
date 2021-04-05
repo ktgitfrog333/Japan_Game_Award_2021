10e9cc198b2c817e94f05b99f6fa47683bed982e
ktgitfrog333 <paraysis+git@gmail.com>
2021-04-05 22:47:58 +0900

    ★ツルツルモードの実装 - 切り替えの実装 　Rボタンでツルツルモードに切り替え 　再度Rボタン押下でツルツルモード解除の処理。（カラマリモードに切り替え）

---
+++ main_scene/CalamariTape/Assets/Scripts/ModeChanger.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+/// <summary>
+/// プレイヤーモードチェンジのスクリプトクラス
+/// <para/>カラマリモード・ネンチャクモード・ツルツルモードへ切り替え
+/// </summary>
+public class ModeChanger : MonoBehaviour
+{
+    /// <summary>カラマリモードオブジェクト</summary>
+    [SerializeField] private GameObject _calamari;
+    /// <summary>ネンチャクモードオブジェクト</summary>
+    [SerializeField] private GameObject _nenchak;
+    /// <summary>ツルツルモードオブジェクト</summary>
+    [SerializeField] private GameObject _tsurutsuru;
+
+    /// <summary>カラマリモード切り替え入力フラグ</summary>
+    private bool _calamariInput;
+    /// <summary>ツルツルモード切り替え入力フラグ</summary>
+    private bool _tsurutsuruInput;
+
+    /// <summary>モード状態</summary>
+    [SerializeField]　private string _mode;
+
+    void Start()
+    {
+        _calamari.SetActive(true);
+        _mode = _calamari.name.ToString();
+    }
+
+    private void Update()
+    {
+        // ループ内で複数のモード切り替え入力が発生しないように制御するフラグ
+        bool inputActive = false;
+
+        // 入力チェック
+        if (_tsurutsuruInput == false && inputActive == false && _mode.Equals(_calamari.name.ToString()))
+        {
+            _calamariInput = false;
+            _tsurutsuruInput = CrossPlatformInputManager.GetButtonDown("RB");
+            inputActive = true;
+
+        }
+        if (_calamariInput == false && inputActive == false && _mode.Equals(_tsurutsuru.name.ToString()))
+        {
+            _tsurutsuruInput = false;
+            _calamariInput = CrossPlatformInputManager.GetButtonDown("RB");
+            inputActive = true;
+        }
+
+        // モード切り替え
+        if (_tsurutsuruInput == true)
+        {
+            TsurutsuruModeEnabledChange();
+        }
+        if (_calamariInput == true)
+        {
+            CalamariModeDisabledChange();
+        }
+    }
+
+    /// <summary>
+    /// カラマリモードへ切り替え
+    /// </summary>
+    private void CalamariModeDisabledChange()
+    {
+        _calamari.SetActive(true);
+        _nenchak.SetActive(false);
+        _tsurutsuru.SetActive(false);
+        _mode = _calamari.name.ToString();
+    }
+
+    /// <summary>
+    /// ネンチャクモードへ切り替え
+    /// </summary>
+    private void NenchakModeEnabledChange()
+    {
+        _calamari.SetActive(false);
+        _nenchak.SetActive(true);
+        _tsurutsuru.SetActive(false);
+        _mode = _nenchak.name.ToString();
+    }
+
+    /// <summary>
+    /// ツルツルモードへ切り替え
+    /// </summary>
+    private void TsurutsuruModeEnabledChange()
+    {
+        _calamari.SetActive(false);
+        _nenchak.SetActive(false);
+        _tsurutsuru.SetActive(true);
+        _mode = _tsurutsuru.name.ToString();
+    }
+}
--- main_scene/ReadMe.txt
+++ main_scene/ReadMe.txt
@@ -1,3 +1,12 @@
+2021/04/05 22:43
+★ツルツルモードの実装 - 切り替えの実装
+・プレイヤー用のオブジェクト作成して、仮の床オブジェクトを配置
+main_scene\CalamariTape\Assets\Scenes\main.unity
+・InputManagerにツルツルモード切り替え名「RB」を追加
+main_scene\CalamariTape\ProjectSettings\InputManager.asset
+・「Rボタンでツルツルモードに切り替え」「再度Rボタン押下でツルツルモード解除の処理。（カラマリモードに切り替え）」
+main_scene\CalamariTape\Assets\Scripts\ModeChanger.cs
+
 2021/04/04 22:50
 ★ReadMeの記載が間違っていた為修正
 
